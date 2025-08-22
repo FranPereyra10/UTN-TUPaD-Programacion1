9535148374dc04d721cb44c3d2348e397fa3f580
FranPereyra10 <francisco.jpereyra10@gmail.com>
2025-08-22 19:13:52 -0300

    EJERCICIO2_TP3

--- Unidad 3/TP_N°3.py
+++ Unidad 3/TP_N°3.py
@@ -6,5 +6,14 @@
 edad = int(input ("Ingrese su edad:"))
 if edad >= 18:
     print ("Es mayor de edad")
-    
 
+#EJERCICIO2
+# Escribir un programa que solicite su nota al usuario. Si la nota es mayor o igual a 6, deberá
+#mostrar por pantalla un mensaje que diga “Aprobado”; en caso contrario deberá mostrar el
+#mensaje “Desaprobado”.
+ 
+nota = int(input("Ingrese su nota: "))
+if nota >= 6:
+    print("APROBADO")
+else:
+    print("DESAPROBADO")
