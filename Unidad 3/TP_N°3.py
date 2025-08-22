91428755335fb9d350394e5e9be0317e76ce4e28
FranPereyra10 <francisco.jpereyra10@gmail.com>
2025-08-22 19:46:29 -0300

    EJERCICIO4_TP3

--- Unidad 3/TP_N°3.py
+++ Unidad 3/TP_N°3.py
@@ -34,3 +34,26 @@ elif numero % 2 != 0:
     else:
         pass 
 
+#EJERCICIO4
+# Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
+#siguientes categorías pertenece:
+#● Niño/a: menor de 12 años.
+#● Adolescente: mayor o igual que 12 años y menor que 18 años.
+#● Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
+#● Adulto/a: mayor o igual que 30 años.
+
+edad = int(input("Por favor ingrese su edad: "))
+#vamos a plantear las condiciones para clasificarlo en un grupo 
+
+if edad > 0 and edad < 12:
+    print ("Usted pertenece al grupo de niños.")
+elif edad >= 12 and edad < 18 :
+    print ("Usted pertenece al grupo de adolescentes.")
+elif edad >= 18 and edad < 30 :
+    print("Usted pertenece al grupo de Adulto Joven.")
+elif edad >= 30:
+    print("Usted pertences al grupo de Adultos.")
+else:
+    print("EDAD INVALIDA.")
+
+
