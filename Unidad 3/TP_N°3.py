474905cdc902f933d4fffcd4e88572e0f75ae312
FranPereyra10 <francisco.jpereyra10@gmail.com>
2025-08-24 16:03:01 -0300

    EJERCICIO7_TP3

--- Unidad 3/TP_N°3.py
+++ Unidad 3/TP_N°3.py
@@ -120,4 +120,24 @@ elif media == moda == mediana :
 else :
     print("La lista no tiene un sesgo definido")
 
+#EJERCICIO7
+
+#Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado
+#termina con vocal, añadir un signo de exclamación al final e imprimir el string resultante por
+#pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por
+#pantalla.
+
+vocales = ["a","e","i","o","u"]
+
+frase = input("Ingrese una frase: ")
+
+ultimaletra = frase.lower()[len(frase)-1]
+
+if ultimaletra in vocales:
+    print (f"{frase}!")
+else:
+    print(frase)
+
+
+
 
