a40f6c7f7f9de5eb707ff4a8786b063ec417724c
FranPereyra10 <francisco.jpereyra10@gmail.com>
2025-09-11 20:19:38 -0300

    TP4_EJERCICIO9

--- Unidad 4/tp4.py
+++ Unidad 4/tp4.py
@@ -117,3 +117,17 @@ print (f"Usted a ingresado {impar} numeros impares")
 print (f"Usted a ingresado {neg} numeros negativos")
 print (f"Usted a ingresado {pos} numeros positivos")   
 print (f"Usted a ingresado el numero cero {cero} veces") 
+
+#EJERCICIO9
+
+#Elabora un programa que permita al usuario ingresar 100 números enteros y luego calcule la
+#media de esos valores. (Nota: puedes probar el programa con una cantidad menor, pero debe
+#poder procesar 100 números cambiando solo un valor).
+
+suma = 0
+media = 0
+for i in range (0,100):
+    num = int(input("Ingrese un numero: "))
+    suma += num
+
+print("La media  de los numeros ingresados es:",suma/100)
