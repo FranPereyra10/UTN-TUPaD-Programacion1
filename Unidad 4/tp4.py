a645040b47db22fb1b8186ac5b4b08361af3f443
FranPereyra10 <francisco.jpereyra10@gmail.com>
2025-09-11 19:05:06 -0300

    TP4_EJERCICIO3

--- Unidad 4/tp4.py
+++ Unidad 4/tp4.py
@@ -21,3 +21,20 @@ while num != 0:
 print (f"La cantidad de digitos del numero ingresado es: {cont}")
 
 #EJERCICIO3
+#Escribe un programa que sume todos los números enteros comprendidos entre dos valores
+#dados por el usuario, excluyendo esos dos valores.
+#Le pedimos al usuario que ingrese dos valores de inicio y fin
+print ("Ingrese dos valores: ")
+num1 = int(input("Numero1: ")) 
+num2 = int(input("Numero2: "))
+sumatoria = 0
+#planteamos un condicional para separar los casos donde el primer numero sea mayor y otro para el caso que sea menor que el segundo
+if num1 < num2:
+    for i in range (num1+1,num2):
+        num = i
+        sumatoria += num
+elif num1 > num2:
+    for i in range (num1-1,num2,-1):
+        num = i
+        sumatoria += num
+print (sumatoria)
