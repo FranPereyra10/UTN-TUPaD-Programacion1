e6b1a227f48b07bef18b495f17dfaf9c06430e22
FranPereyra10 <francisco.jpereyra10@gmail.com>
2025-09-23 21:43:49 -0300

    TP5_EJERCICIO3

--- Unidad 5/tp5.py
+++ Unidad 5/tp5.py
@@ -31,3 +31,28 @@ print(f"Los productos ingresados son: {productos} ")
 
 productos.remove(input("ingrese el producto que desea eliminar: "))
 print(productos)
+
+#EJERCICIO 3
+
+#Generar una lista con 15 números enteros al azar entre 1 y 100.
+#• Crear una lista con los pares y otra con los impares.
+#• Mostrar cuántos números tiene cada lista.
+numeros_pares = []
+numeros_impares = []
+
+import random
+
+numeros_aleatorios = [random.randint(1,100) for i in range (15)]
+print(numeros_aleatorios)
+
+for i in range (15):
+    if numeros_aleatorios[i] % 2 == 0:
+        numeros_pares.append(numeros_aleatorios[i])
+    elif numeros_aleatorios[i] % 2 != 0:
+        numeros_impares.append(numeros_aleatorios[i])
+
+print(f"Los numeros pares de la lista son: {numeros_pares}")
+print(f"La lista posee {len(numeros_pares)} numeros pares")
+print(f"Los numeros impares de la lista son: {numeros_impares}")
+print(f"La lista posee {len(numeros_impares)} numeros impares")
+
