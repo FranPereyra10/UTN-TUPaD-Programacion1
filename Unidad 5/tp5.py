59c0a537dfd53e8816f41b29405f929a561b9685
FranPereyra10 <francisco.jpereyra10@gmail.com>
2025-09-23 21:21:32 -0300

    EJERCICIO2_TP5

--- Unidad 5/tp5.py
+++ Unidad 5/tp5.py
@@ -16,3 +16,18 @@ print(lista)
 print(f"El promedio de las notas ingresadas es: {promedio}")
 print(f"La nota maxima ingresada es: {maximo}")
 print(f"La nota minima ingresada es: {minimo}")
+
+#EJERCICIO 2
+#2) Pedir al usuario que cargue 5 productos en una lista.
+#• Mostrar la lista ordenada alfabéticamente. Investigue el uso del método sorted().
+#• Preguntar al usuario qué producto desea eliminar y actualizar la lista.
+
+productos = []
+
+for i in range (5):
+    productos.append(input("Ingrese un producto: "))
+    productos=sorted(productos)
+print(f"Los productos ingresados son: {productos} ")
+
+productos.remove(input("ingrese el producto que desea eliminar: "))
+print(productos)
